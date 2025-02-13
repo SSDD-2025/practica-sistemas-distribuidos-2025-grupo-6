287ca047e320b33ebfc2467e5eb5e4d10df7b88d
GranLobo2004 <146849588+GranLobo2004@users.noreply.github.com>
2025-02-13 18:48:14 +0100

    Creation of productController and modification of ProductService

---
+++ src/main/java/es/dlj/onlinestore/controller/ProductController.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+package es.dlj.onlinestore.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import es.dlj.onlinestore.model.Product;
+import es.dlj.onlinestore.service.ProductService;
+
+@Controller
+class ProductController {
+
+    @Autowired
+    private ProductService productService;
+
+    @GetMapping("/product/{id}")
+    public String loadProductDetails(Model model, @PathVariable Long id){
+
+        model.addAttribute("product", productService.getProduct(id));
+
+        return "productDetailed_template";
+    }
+
+}
