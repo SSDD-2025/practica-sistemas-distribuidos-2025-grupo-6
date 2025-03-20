2fd9b09430873d94e8e8754275320c9bce6d83f7
GranLobo2004 <146849588+GranLobo2004@users.noreply.github.com>
2025-03-20 09:19:23 +0100

    NoSuchElementExceptionControllerAdvice file

---
+++ src/main/java/es/dlj/onlinestore/NoSuchElementExceptionControllerAdvice.java
@@ -0,0 +1,17 @@
+package es.dlj.onlinestore;
+
+import java.util.NoSuchElementException;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ControllerAdvice
+public class NoSuchElementExceptionControllerAdvice {
+
+	@ResponseStatus(HttpStatus.NOT_FOUND)
+	@ExceptionHandler(NoSuchElementException.class)
+	public void handleNotFound() {
+	}
+}
