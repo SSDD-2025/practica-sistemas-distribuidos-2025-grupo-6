4b75a6bd38086fa9019b988964dd16de21b76b60
David Pimentel <davidpimentelmontes@gmail.com>
2025-03-31 20:17:25 +0200

    Preliminar product rest controller implementation

---
+++ src/main/java/es/dlj/onlinestore/controller/rest/ProductRestController.java
@@ -0,0 +1,94 @@
+package es.dlj.onlinestore.controller.rest;
+
+import java.util.Collection;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import es.dlj.onlinestore.dto.ProductDTO;
+import es.dlj.onlinestore.dto.ProductSimpleDTO;
+import es.dlj.onlinestore.dto.ReviewDTO;
+import es.dlj.onlinestore.service.ProductService;
+import es.dlj.onlinestore.service.ReviewService;
+import es.dlj.onlinestore.service.UserService;
+import jakarta.validation.Valid;
+
+@RestController
+@RequestMapping("/api/products")
+public class ProductRestController {
+
+    @Autowired
+    private ProductService productService;
+
+    @Autowired
+    private ReviewService reviewService;
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("/")
+    public ResponseEntity<Collection<ProductSimpleDTO>> getAllProducts(
+            @RequestParam(required = false) String name,
+            @RequestParam(required = false) List<String> productTypes,
+            @RequestParam(required = false) Integer minPrice,
+            @RequestParam(required = false) Integer maxPrice,
+            @RequestParam(required = false) List<String> tags) {
+        Collection<ProductSimpleDTO> products = productService.findAllDTOsBy(name, minPrice, maxPrice, tags, productTypes);
+        return ResponseEntity.ok(products);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ProductDTO> getProductById(@PathVariable Long id) {
+        ProductDTO product = productService.findDTOById(id);
+        return ResponseEntity.ok(product);
+    }
+
+    @PostMapping("/")
+    public ResponseEntity<ProductDTO> createProduct(
+            @RequestParam List<MultipartFile> imagesVal,
+            @RequestParam String tagsVal,
+            @Valid @RequestBody ProductDTO newProductDTO) {
+        ProductDTO savedProductDTO = productService.saveProduct(newProductDTO, imagesVal, tagsVal);
+        return ResponseEntity.status(201).body(savedProductDTO);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<ProductDTO> updateProduct(
+            @PathVariable Long id,
+            @RequestParam List<MultipartFile> imagesVal,
+            @RequestParam String tagsVal,
+            @Valid @RequestBody ProductDTO updatedProductDTO) {
+        productService.updateProduct(id, updatedProductDTO, imagesVal, tagsVal);
+        return ResponseEntity.ok(updatedProductDTO);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteProduct(@PathVariable Long id) {
+        if (productService.isProductOwner(id)) {
+            productService.deleteProduct(id);
+            return ResponseEntity.noContent().build();
+        }
+        return ResponseEntity.status(403).build();
+    }
+
+    @PostMapping("/{id}/reviews/")
+    public ResponseEntity<Void> addReview(@PathVariable Long id, @Valid @RequestBody ReviewDTO reviewDTO) {
+        reviewService.save(id, reviewDTO);
+        return ResponseEntity.status(201).build();
+    }
+
+    @DeleteMapping("/{productId}/reviews/{reviewId}")
+    public ResponseEntity<Void> deleteReview(@PathVariable Long productId, @PathVariable Long reviewId) {
+        reviewService.delete(reviewId);
+        return ResponseEntity.noContent().build();
+    }
+
+    @PostMapping("/{id}/add-to-cart")
+    public ResponseEntity<Void> addProductToCart(@PathVariable Long id) {
+        userService.addProductToCart(id);
+        return ResponseEntity.status(201).build(); // Created
+    }
+}
