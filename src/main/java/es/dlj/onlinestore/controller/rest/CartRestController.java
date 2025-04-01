f6e9e4881b6f43f847aad553fa097f9439af7498
GranLobo2004 <146849588+GranLobo2004@users.noreply.github.com>
2025-04-01 19:00:28 +0200

    Cart rest controller

---
+++ src/main/java/es/dlj/onlinestore/controller/rest/CartRestController.java
@@ -0,0 +1,62 @@
+package es.dlj.onlinestore.controller.rest;
+
+import java.net.URI;
+import java.util.Collection;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriBuilder;
+import org.springframework.web.util.UriComponentsBuilder;
+
+
+import static org.springframework.web.servlet.support.ServletUriComponentsBuilder.fromCurrentRequest;
+
+import es.dlj.onlinestore.dto.OrderSimpleDTO;
+import es.dlj.onlinestore.dto.ProductSimpleDTO;
+import es.dlj.onlinestore.service.OrderService;
+import es.dlj.onlinestore.service.UserService;
+
+@RestController
+@RequestMapping("/api/cart")
+public class CartRestController {
+
+    @Autowired
+    private UserService userService;
+
+    @Autowired OrderService orderService;
+
+    @GetMapping
+    public Collection<ProductSimpleDTO> showCart(){
+        return userService.getLoggedUserDTO().cartProducts();
+    }
+
+    @DeleteMapping("/product/{id}")
+    public void removeProduct(@PathVariable Long id){
+        userService.removeProductFromCart(id);
+        //TODO: return statement;
+    }
+
+    @DeleteMapping("/all")
+    public void clearCart(){
+        userService.clearCart();
+        //TODO: return statement;
+    }
+
+    @PostMapping("/order")
+    public ResponseEntity<OrderSimpleDTO> createOrder(@RequestBody String paymentMethod, String address, String phoneNumber, UriComponentsBuilder  uriBuilder){
+        OrderSimpleDTO order = orderService.proceedCheckout(paymentMethod, address, phoneNumber);
+        if (order == null) return ResponseEntity.noContent().build();
+        URI location =  uriBuilder.path("/api/profile/order/{id}").buildAndExpand(order.id()).toUri();
+        return ResponseEntity.created(location).body(order);
+    }
+
+
+    
+}
